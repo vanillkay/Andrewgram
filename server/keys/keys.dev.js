beb6deed721f894e41de846d9401599afd6cca9e
Andrew Shkurenko <andrey.shkurenko@icloud.com>
2021-03-31 13:28:39 +0300

    Delete some files

--- server/keys/keys.dev.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    MONGODB_URI: 'mongodb+srv://Andrew:hardrock1@cluster0.gxkyq.mongodb.net/andrewgram',
-    SESSION_SECRET: 'some secret value',
-    PORT: process.env.PORT || 5000,
-    SENDGRID_API_KEY: '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',
-    EMAIL_FROM: 'Andrewgram <vanillkay@gmail.com>',
-    BASE_URL: 'http://localhost:3000',
-    TYPE: 'DEVs'
-}
